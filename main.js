3ecb370a0a8ea385ec73db17c0f196095e59cf5f
deus <deus@192.168.1.7>
2022-10-30 18:34:26 -0500

    Set project variables and basic html boilerplate

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// ======================= Grab elements ============================
+
+// Main title
+let mainTitle = document.getElementById('main-title');
+
+// Input number
+let inputNum = document.getElementById('inputNum');
+
+// Convert button
+let btnEl = document.getElementById('btnEl');
+
+// Titles
+let titleOne = document.getElementById('title-one');
+let titleTwo = document.getElementById('title-two');
+let titleThree = document.getElementById('title-three');
+
+// Paragraphs
+let pElOne = document.getElementById('p-el-one');
+let pElTwo = document.getElementById('p-el-two');
+let pElThree = document.getElementById('p-el-three');
+
+// =================================================================
+
